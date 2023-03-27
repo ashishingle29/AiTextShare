0205a3d9d2b88de571bd43b887144978233267b5
ashishingle29 <ashishingle589@gmail.com>
2023-03-27 17:07:56 +0530

    frontend

--- Backend/db/mdb.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-const db = process.env.DATABASE
-
-mongoose.connect(db)
-    .then(() => { console.log("MongoDB is Connected.....") })
-    .catch((error) => { console.log(error.message) })
